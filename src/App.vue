a90305d23ddbc002f0b6765b9ada9bd0750e1f1d
mehrdadsamadi <samadimehrdad49@gmail.com>
2024-08-17 22:36:24 +0330

    first commit | satrt project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <CategoriesTree />
+  </div>
+</template>
+
+<script>
+import CategoriesTree from "./components/CategoriesTree.vue"
+
+export default {
+  name: 'App',
+  components: {
+    CategoriesTree
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>

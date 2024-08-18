68ad758dfe5d46f4b9a156e0aec8e16822e40caf
mehrdadsamadi <samadimehrdad49@gmail.com>
2024-08-18 10:06:38 +0330

    complete project

--- src/components/CategoryCheckbox.vue
+++ src/components/CategoryCheckbox.vue
@@ -1,68 +1,96 @@
 <template>
-    <div>
-        <label class="flex items-center flex-row-reverse gap-2">
-            <input
-                type="checkbox"
-                :checked="checked"
-                :indeterminate.prop="indeterminate"
-                @change="toggleCheck"
-            />
-            <span class="font-semibold">{{ node.name }}</span>
-        </label>
-        <div v-if="node.children" class="me-8">
-            <CategoryCheckbox
-                v-for="cat in node.children" 
-                :key="cat.id"
-                :node="cat"
-                @update="toggleCheck"
-            />
-        </div>
+  <div>
+    <label class="flex items-center flex-row-reverse gap-2">
+      <input
+        type="checkbox"
+        :checked="checked"
+        :indeterminate.prop="indeterminate"
+        @change="toggleCheck"
+      />
+      <span class="font-semibold">{{ node.name }}</span>
+    </label>
+    <div v-if="node.children" class="me-8">
+      <CategoryCheckbox
+        v-for="cat in node.children"
+        :key="cat.id"
+        :node="cat"
+        @update="updateParent"
+      />
     </div>
-  </template>
-  
-<script>
+  </div>
+</template>
 
-  export default {
-    name:"CategoryCheckbox",
-    data() {
-      return {
-        checked: false,
-        indeterminate: false,
-      };
+<script>
+export default {
+  name: "CategoryCheckbox",
+  props: {
+    node: {
+      type: Object,
+      required: true,
     },
-    props: {
-        node: {
-          type: Object,
-          required: true,
-        },
+  },
+  data() {
+    return {
+      checked: this.node.checked || false,
+      indeterminate: false,
+    };
+  },
+  watch: {
+    'node.checked'(val) {
+      this.checked = val;
     },
-    computed: {
-        isIndeterminate() {
-          return this.node.children && this.node.children.some(child => child.checked !== this.checked);
-        },
+    checked(val) {
+      this.$set(this.node, 'checked', val);
+      this.updateIndeterminate();
     },
-    methods: {
-        toggleCheck() {
-          this.checked = !this.checked;
-          this.updateChildren(this.node.children, this.checked);
-          this.$emit('update');
-        },
-        updateChildren(children, checked) {
-          if (children) {
-            children.forEach(child => {
-              child.checked = checked;
-              if (child.children) {
-                this.updateChildren(child.children, checked);
-              }
-            });
-          }
-        },
-    },
-    watch: {
-        checked() {
-          this.indeterminate = this.isIndeterminate;
-        },
+    'node.children': {
+      handler() {
+        this.updateIndeterminate();
+        this.updateParent();
       },
-  };
+      deep: true
+    }
+  },
+  computed: {
+    isIndeterminate() {
+      if (!this.node.children || this.node.children.length === 0) {
+        return false;
+      }
+      const checkedCount = this.node.children.filter(child => child.checked).length;
+      return checkedCount > 0 && checkedCount < this.node.children.length;
+    },
+  },
+  methods: {
+    toggleCheck() {
+      const newValue = !this.checked;
+      this.checked = newValue;
+      this.updateChildren(this.node.children, newValue);
+      this.$emit('update');
+    },
+    updateChildren(children, checked) {
+      if (children) {
+        children.forEach(child => {
+          this.$set(child, 'checked', checked);
+          if (child.children) {
+            this.updateChildren(child.children, checked);
+          }
+        });
+      }
+    },
+    updateParent() {
+      if (!this.node.children) return;
+      const allChecked = this.node.children.every(child => child.checked);
+      this.checked = allChecked;
+      this.$set(this.node, 'checked', allChecked);
+      this.updateIndeterminate();
+      this.$emit('update');
+    },
+    updateIndeterminate() {
+      this.indeterminate = this.isIndeterminate;
+    }
+  },
+  mounted() {
+    this.updateIndeterminate();
+  }
+};
 </script>
-  
